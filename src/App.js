f338fcac0253d130ab007b9308587833352057be
Alan BIju Gregory <bijualan669@gmail.com>
2023-03-28 17:22:19 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Footer from './footer';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Helo</h1> 
+       <form>
+
+        </form>   
       </header>
+      <Footer thom='Copyright 2023'></Footer>
     </div>
   );
 }
